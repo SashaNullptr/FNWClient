75bf941ca8a42727cade51ce037749c42d1a2057
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-02-27 14:28:19 -0700

    Added setup.py skeleton.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fnw_client',
+    version='0.1.0',
+    packages=find_packages(exclude=[ 'docs' ]),
+    install_requires=['numpy',
+                      'scipy',
+                      'pandas',
+                      'telethon'],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: Linux',
+        'Topic :: Natural Language Processing',
+    ),
+    author='Sasha E. Fox'
+)
