564ad21e065ca8f4df66fa47eeebf745b06a0c48
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-10-13 22:07:59 -0600

    Outline of API endpoint implementation.

---
+++ fnwclient/endpoint/endpoint.py
@@ -0,0 +1,32 @@
+# System
+import json
+
+# Third Party
+from flask import Blueprint, request, Response, jsonify
+from injector import inject
+
+# Local
+
+blueprint = Blueprint('faces', __name__)
+
+@blueprint.route('/health', methods=['GET'])
+@inject
+def example_endpoint( event=None, context=None ):
+    """
+    Healthcheck for the service
+
+    An example request might look like:
+    .. sourcecode:: http
+       GET www.x.com/example HTTP/1.1
+       Host: example.com
+       Accept: application/json, text/javascript
+
+    Results will be returned as JSON object with the following format:
+
+    .. code-block:: json
+        {
+          "healthy": true
+        }
+    """
+
+    return Response( json.dumps({'healthy':True}), 200, mimetype='application/json' )
