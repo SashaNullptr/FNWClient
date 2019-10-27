32249825b8dc0f36f122d5209a060697631c4383
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-10-27 11:24:17 -0700

    Expose port 8080

--- services/server.py
+++ services/server.py
@@ -6,4 +6,4 @@ from services.fnwclient import app
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.INFO)
 
-serve(app, host='0.0.0.0', port=80)
+serve(app, host='0.0.0.0', port=8080)
