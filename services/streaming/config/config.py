356ca9bed4bba7916960187b666f60e864ffd3ed
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-10-30 14:29:55 -0700

    Generalized config variable names

--- services/streaming/config/config.py
+++ services/streaming/config/config.py
@@ -3,22 +3,22 @@ import os
 from configparser import ConfigParser
 
 ROOT_DIR = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-API_CONFIG = os.path.join(ROOT_DIR, 'api.ini')
+API_CONFIG = os.path.join(ROOT_DIR, 'credentials.ini')
 
 
-def config(filename=None, section='api'):
+def config(filename=None, section):
 
     filename = filename if filename is not None else API_CONFIG
 
     parser = ConfigParser()
     parser.read(filename)
 
-    api_info = {}
+    info = {}
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
-            api_info[param[0]] = param[1]
+            info[param[0]] = param[1]
     else:
         raise FileNotFoundError('Section {0} not found in the {1} file'.format(section, filename))
 
-    return api_info
+    return info
