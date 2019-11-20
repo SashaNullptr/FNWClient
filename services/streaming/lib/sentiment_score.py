589413185411584ce8f06aaf04b6b564a8c5b33c
Sasha Elaine Fox <sashanullptr@gmail.com>
2019-11-20 01:33:09 -0700

    Use paths over strings.

--- services/streaming/lib/sentiment_score.py
+++ services/streaming/lib/sentiment_score.py
@@ -1,4 +1,5 @@
-from os import environ
+from os import getenv
+from pathlib import Path
 
 from services.streaming.lib.text_blob_sentiment import text_sentiment
 from services.streaming.lib.flair_sentiment import FlairTextSentiment
@@ -11,14 +12,15 @@ logger = logging.getLogger(__name__)
 class SentimentScoreModel:
 
     def __init__(self):
-        model_location = environ.getenv('SENTIMENT_MODEL', "/opt/models/sentiment/best-model.pt")
+        model_location = getenv('SENTIMENT_MODEL', "/opt/models/sentiment/best-model.pt")
+        model_path = Path(model_location)
 
-        if not model_location.exists():
-            logger.warning("Couldn't find Flair model at " + str(model_location) + " using TextBlob instead.")
+        if not model_path.exists():
+            logger.warning("Couldn't find Flair model at " + str(model_path) + " using TextBlob instead.")
             self.__score_function = text_sentiment
         else:
-            logger.warning("Using Flair to compute sentiment score with model loaded from " + str(model_location) )
-            self.__model = FlairTextSentiment(model_location)
+            logger.warning("Using Flair to compute sentiment score with model loaded from " + str(model_path) )
+            self.__model = FlairTextSentiment(model_path)
             self.__score_function = self.__model.text_sentiment
 
     def sentiment_score(self, raw_text):
